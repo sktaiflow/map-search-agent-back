9af235d23ae4d4287cc07f947eb167442b2e157d
leejaehwan <smart0924@gmail.com>
2025-08-18 21:43:02 +0900

    add HTTPBaseClient

--- app/clients/__init__.py
+++ app/clients/__init__.py
@@ -1,4 +1,10 @@
 from .map import MAPClient
-from .http_base import build_http_client, InvalidHttpStatus, HTTPBaseClientResponse
+from .http_base import build_http_client, InvalidHttpStatus, HTTPBaseClientResponse, HTTPBaseClient
 
-__all__ = ["MAPClient", "build_http_client", "InvalidHttpStatus", "HTTPBaseClientResponse"]
+__all__ = [
+    "MAPClient",
+    "build_http_client",
+    "InvalidHttpStatus",
+    "HTTPBaseClientResponse",
+    "HTTPBaseClient",
+]
