b075ba571c95a47bbb477ceca0186d308846b575
leejaehwan <smart0924@gmail.com>
2025-08-19 15:49:31 +0900

    Add container

--- app/container/__init__.py
+++ app/container/__init__.py
@@ -2,12 +2,15 @@ from dependency_injector import containers, providers
 
 from .graphs import GraphContainer
 from .agents import AgentContainer
+from .db import PGVectorDBContainer
+from .clients import ClientContainer
 
-__all__ = ["GraphContainer", "AgentContainer"]
+__all__ = ["GraphContainer", "AgentContainer", "PGVectorDBContainer", "ClientContainer"]
 
 
 class Container(containers.DeclarativeContainer):
-    graphs: GraphContainer = providers.Container(GraphContainer)
-    agents: AgentContainer = providers.Container(AgentContainer, graphs=graphs)
-
+    # graphs: GraphContainer = providers.Container(GraphContainer)
+    # agents: AgentContainer = providers.Container(AgentContainer, graphs=graphs)
+    pgvector_db: PGVectorDBContainer = providers.Container(PGVectorDBContainer)
+    clients: ClientContainer = providers.Container(ClientContainer)
     wiring_config = containers.WiringConfiguration(packages=["app"])
