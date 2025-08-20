125120710a9f69b34f05d56ea3358c8a9e6f9077
leejaehwan <smart0924@gmail.com>
2025-08-20 10:59:50 +0900

    add container

--- app/container/__init__.py
+++ app/container/__init__.py
@@ -4,13 +4,24 @@ from .graphs import GraphContainer
 from .agents import AgentContainer
 from .db import PGVectorDBContainer
 from .clients import ClientContainer
+from .llm import LLMContainer
+from .toolkit import ToolkitContainer
 
-__all__ = ["GraphContainer", "AgentContainer", "PGVectorDBContainer", "ClientContainer"]
+__all__ = [
+    "GraphContainer",
+    "AgentContainer",
+    "PGVectorDBContainer",
+    "ClientContainer",
+    "LLMContainer",
+    "ToolkitContainer",
+]
 
 
 class Container(containers.DeclarativeContainer):
-    # graphs: GraphContainer = providers.Container(GraphContainer)
-    # agents: AgentContainer = providers.Container(AgentContainer, graphs=graphs)
+    graphs: GraphContainer = providers.Container(GraphContainer)
     pgvector_db: PGVectorDBContainer = providers.Container(PGVectorDBContainer)
     clients: ClientContainer = providers.Container(ClientContainer)
+    agents: AgentContainer = providers.Container(AgentContainer, graphs=graphs)
+    llm: LLMContainer = providers.Container(LLMContainer)
+    toolkit: ToolkitContainer = providers.Container(ToolkitContainer)
     wiring_config = containers.WiringConfiguration(packages=["app"])
