2aa1a37489cd96a5ccd1e7154f1d5b63d1f7ddcc
leejaehwan <smart0924@gmail.com>
2025-08-20 11:04:10 +0900

    add SynonymClient

--- app/clients/__init__.py
+++ app/clients/__init__.py
@@ -1,8 +1,10 @@
 from .map import MAPClient
+from .synonym import SynonymClient
 from .http_base import build_http_client, InvalidHttpStatus, HTTPBaseClientResponse, HTTPBaseClient
 
 __all__ = [
     "MAPClient",
+    "SynonymClient",
     "build_http_client",
     "InvalidHttpStatus",
     "HTTPBaseClientResponse",
