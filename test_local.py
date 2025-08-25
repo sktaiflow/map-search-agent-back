79f2bdd391a861b1e2197417711fd95660df5693
leejaehwan <smart0924@gmail.com>
2025-08-25 13:16:53 +0900

    feat: not used file only for test

--- test_local.py
+++ test_local.py
@@ -11,27 +11,41 @@ from langchain_core.messages import (
     SystemMessage,
     FunctionMessage,
 )
+from langgraph.prebuilt import create_react_agent
+from app.llms.llm import LLMClient
+
+from langchain_openai import ChatOpenAI
+from app.core.prompts import PLANNING_PROMPT
+
+OPENAI_API_BASE = "https://aide.dev.apollo-lunar.com/pe-proxy/api/v1/compatible/openai"
+# OPENAI_BASE_URL=https://aihub-api.sktelecom.com/aihub/v2/sandbox
+OPENAI_API_KEY = "e97ee307-a791-4e06-ade1-df4b9d032eed"
+from pydantic import SecretStr
+
+LLM_CLIENT = LLMClient(
+    ChatOpenAI(base_url=OPENAI_API_BASE, api_key=OPENAI_API_KEY),
+    max_retries=1,
+)
+messages = [
+    SystemMessage(content=PLANNING_PROMPT.format(tool_list_json="test")),
+    HumanMessage(content=f"사용자 쿼리: 요금제"),
+]
 
 
-def test_llm_local():
-    """로컬 설정을 읽어서 LLM을 호출하는 테스트"""  # 1. 설정 로드 테스트
-    test_messages = [
-        HumanMessage(content="현재 수행할 단계에 맞게 유저의 요청사항에 맞는 tool을 선택해 주세요.")
-    ]
-    try:
-        response = create_chat_completion(
-            messages=test_messages, model="124252", is_result=False, timeout=30  # 중간 과정
-        )
-        print("##########")
-        print(response)
-    except Exception as e:
-        print(e)
+async def test_llm_local():
+    resp = await LLM_CLIENT.acomplete(messages=messages, model="124252", timeout=30)
+    print(resp)
+    return resp
+
+
+result = asyncio.run(test_llm_local())
 
 
 def main():
     """메인 함수"""
-    test_llm_local()
+    result = await test_llm_local()  # 결과 받기
+    print("최종 결과:", result)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
