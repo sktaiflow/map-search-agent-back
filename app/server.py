30ba643110b10099a347f4b6bdd409cbc04910d0
leejaehwan <smart0924@gmail.com>
2025-08-25 11:07:46 +0900

    feat: Map Search Agent api server [base version - v.0.0.1]

--- app/server.py
+++ app/server.py
@@ -25,3 +25,51 @@ from configs import StackType, PHASE
 
 
 KST = ZoneInfo("Asia/Seoul")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        app.container = Container()
+        await app.container.init_resources()
+        app.state.ready = True
+    except Exception as e:
+        logger.error(f"Error during app startup: {e}", exc_info=True)
+    yield
+    try:
+        app.state.ready = False
+        await app.container.shutdown_resources()
+    except Exception as e:
+        print(f"Error during app shutdown: {e}")
+
+
+async def healthcheck(request: Request):
+    if not getattr(request.app.state, "ready", False):
+        return Response(status_code=503, media_type="text/plain", content="NOT READY")
+    return Response(media_type="text/plain", content="OK")
+
+
+app = FastAPI(
+    title="Map Search Agent API",
+    version=config.app_version,
+    description=config.api_description,
+    docs_url="/docs",
+    openapi_url="/openapi.json",
+    lifespan=lifespan,
+)
+
+app.add_api_route(path="/api/healthcheck", endpoint=healthcheck)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(middleware_class=BaseHTTPMiddleware, dispatch=request_response_handler)
+app.add_middleware(middleware_class=BaseHTTPMiddleware, dispatch=common_middleware)
+app.add_exception_handler(Exception, error_handler)
+app.add_exception_handler(RequestValidationError, error_handler)
+app.add_exception_handler(HTTPException, error_handler)
+
+app.include_router(api.agents.router)
