1dc406e7823fbac9291c73b9f4a23e68d5a73afa
leejaehwan <smart0924@gmail.com>
2025-08-25 12:51:42 +0900

    fix: refactoring toolkit Container ( individual map tool -> MAPTOOLS )

--- app/container/toolkit.py
+++ app/container/toolkit.py
@@ -1,23 +1,25 @@
 from dependency_injector import containers, providers
 
 from app.core.tools.map import (
-    GetContractMobileContractDevicesTool,
-    GetPlanAddOnAddOnSubscriptionsTool,
+    ContractToolKit,
+    PlanToolKit,
+    MAPTools,
 )
 
 from configs import config as global_config
 
 
+# TODO meta 검색 툴 [cypher agent] 추가 주입 필요
 class ToolkitContainer(containers.DeclarativeContainer):
     clients = providers.DependenciesContainer()
 
-    get_contract_mobile_contract_devices = providers.Factory(
-        GetContractMobileContractDevicesTool,
+    map_toolkit = providers.Factory(
+        MAPTools,
         map_client=clients.map_api,
-        method_api_key=global_config.map_method_api_key_contract_mobile_device,
+        method_api_key=global_config.map_method_api_keys,
     )
-    get_plan_add_on_add_on_subscriptions = providers.Factory(
-        GetPlanAddOnAddOnSubscriptionsTool,
-        map_client=clients.map_api,
-        method_api_key=global_config.map_method_api_key_plan_add_on,
+
+    map_tools = providers.Factory(
+        lambda toolkit: toolkit.get_valid_tools(),
+        toolkit=map_toolkit,
     )
