6960ad0eff51a47c780c4c70a6bd4b124441868a
leejaehwan <smart0924@gmail.com>
2025-08-27 23:01:35 +0900

    temp: 주석처리 (neo4j 미완성 모듈)

--- app/container/__init__.py
+++ app/container/__init__.py
@@ -20,14 +20,14 @@ __all__ = [
 
 class Container(containers.DeclarativeContainer):
     pgvector_db = providers.Container(PGVectorDBContainer)
-    neo4j_db = providers.Container(Neo4jContainer)
+    # neo4j_db = providers.Container(Neo4jContainer)
     clients = providers.Container(ClientContainer)
     llm = providers.Container(LLMContainer)
     toolkit = providers.Container(ToolkitContainer, clients=clients)
     graphs = providers.Container(
         GraphContainer,
         client_container=clients,
-        neo4j_container=neo4j_db,
+        # neo4j_container=neo4j_db,
         toolkit_container=toolkit,
         pgvector_container=pgvector_db,
     )
