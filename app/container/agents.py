7cc7500795c98b66881e11635ca8ee6c566923ad
jiwoon-ha <jiwoonh@gmail.com>
2025-08-28 17:15:37 +0900

    Update agents.py

--- app/container/agents.py
+++ app/container/agents.py
@@ -11,7 +11,6 @@ class AgentContainer(containers.DeclarativeContainer):
     ## http client, graph 주입
     graphs = providers.DependenciesContainer()
     http_client = providers.DependenciesContainer()
-    pgvector_container = providers.DependenciesContainer()
 
     map_agent = providers.Singleton(
         MapSearchAgent,
