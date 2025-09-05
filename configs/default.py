26feb060216f32c7165cb0288f6be6c4d25d9183
leejaehwan <smart0924@gmail.com>
2025-09-05 21:10:45 +0900

    temp: no change

---
+++ configs/default.py
@@ -0,0 +1,100 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import ClassVar, Any
+
+from utils.enums import StrEnum
+
+
+class StackType(StrEnum):
+    PRD = "prd"
+    STG = "stg"
+    DEV = "dev"
+    LOCAL = "local"
+
+
+class BaseConfig(BaseSettings):
+    api_description: ClassVar[
+        str
+    ] = """
+        """
+
+    model_config = SettingsConfigDict(
+        env_file=f".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+    app_name: str = os.environ.get("APP_NAME", "map-search-agent")
+    stack_type: str = os.environ.get("STACK_TYPE", StackType.LOCAL)
+    healthcheck_url: str = os.environ.get("HEALTHCHECK_URL", "/api/healthcheck")
+
+    api_version: str = "no_version_specified"
+    app_version: str = "0.0.1"
+
+    # llm
+    llm_model: str = "gpt-4o"
+    fast_llm_model: str = "gpt-4o-mini"
+    reasoning_llm_model: str = "gpt-o3"
+
+    # map api config
+    map_base_url: str = ""
+    map_api_key: str = ""
+
+    map_method_api_key_contract_mobile_device: str = ""
+    map_method_api_key_account_payment: str = ""
+    map_method_api_key_plan_benefit: str = ""
+    map_method_api_key_rate_usage: str = ""
+    map_method_api_key_data_refill: str = ""
+    map_method_api_key_contract_customer: str = ""
+    map_method_api_key_plan_add_on: str = ""
+    map_method_api_key_plan_basic: str = ""
+    map_method_api_key_rate_limit: str = ""
+    map_method_api_key_data_gift: str = ""
+    map_method_api_key_contract_mobile: str = ""
+    map_method_api_key_account_bill: str = ""
+
+    map_method_api_keys: dict = {
+        "ContractToolKit": "",  # contract 관련
+        "PlanToolKit": "",  # plan 관련
+    }
+
+    # synonym api config
+    synonym_base_url: str = ""
+    synonym_api_key: str = ""
+
+    # postgres config
+    # vector_store_host: str
+    # vector_store_port: int = 5432
+    # vector_store_user: str
+    # vector_store_password: str
+
+    # pg vector store config
+    vector_store_collection_name: str = "map-vector-store"
+    vector_store_provider: str = "pgvector"
+    vector_store_dbname: str = "map_db_vector_store"
+    vector_store_embedding_model_name: str = "text-embedding-3-small"
+    vector_store_embedding_model_dims: int = 1536
+    vector_store_diskann: bool = False
+    vector_store_hnsw: bool = True
+
+    postgres_db_host: str
+    postgres_db_port: int = 5432
+    postgres_db_username: str
+    postgres_db_password: str
+
+    # pg vector
+    m: int = 16
+    ef_construction: int = 64
+
+    # smart bee
+    openai_api_base: str = ""
+    openai_api_key: str = ""
+    aws_region: str = "ap-northeast-2"
+
+    # neo4j
+    neo4j_nlb_dns: str = ""
+    neo4j_bolt_port: str = "7687"
+    neo4j_username: str = ""
+    neo4j_password: str = ""
