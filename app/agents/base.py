1897121a0d39fdb438ffed6f67ccb8abaf820cf4
leejaehwan <smart0924@gmail.com>
2025-09-05 21:02:03 +0900

    feat: implement BaseAgent class [Parent class for map search agent]

---
+++ app/agents/base.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from app import logger
+from app.graph.base import BaseGraph
+from app.schemas.api.schema import InvokeRequest, InvokeResponse
+
+from langchain_core.runnables import RunnableConfig
+from abc import abstractmethod
+from typing import Any
+from uuid import UUID, uuid4
+from abc import abstractmethod
+
+DEFAULT_ALLOWED_EVENTS: list[tuple[str, str]] = [
+    ("LangGraph", "on_chain_end"),
+]
+
+
+class BaseAgentConfig(BaseModel):
+    agent_name: str
+    allowed_events: list[tuple[str, Any]] = DEFAULT_ALLOWED_EVENTS
+
+
+class BaseAgent:
+    """
+    모든 Agent의 공통 기능을 제공하는 베이스 클래스
+    """
+
+    config: BaseAgentConfig = BaseAgentConfig(agent_name="base-agent")
+
+    def __init__(self, graph: BaseGraph) -> None:
+        self.graph = graph
+
+    def invoke(self, input_data: dict[str, Any]) -> Any:
+        """일반 실행 방식"""
+        return self.graph.compiled_graph.invoke(input_data)
+
+    @abstractmethod
+    async def ainvoke(
+        self, input_data: InvokeRequest, runnable_config: RunnableConfig
+    ) -> InvokeResponse:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def preprocess(self, user_input):
+        raise NotImplementedError
+
+    @abstractmethod
+    def postprocess(self, response):
+        """
+        graph 의 invoke 결과를 가공하고자 할 때 사용합니다.
+        Args:
+            content: graph 의 invoke 결과
+        Returns:
+            가공된 메시지
+        """
