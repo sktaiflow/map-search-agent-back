917a087c803fb073bb3b55365512ea7335abec2d
leejaehwan <smart0924@gmail.com>
2025-09-05 21:16:32 +0900

    temp: no change

---
+++ utils/enums.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class StrEnum(str, Enum):
+    def _generate_next_value_(self, start, count, last_values):
+        return self
+
+    def __repr__(self) -> str:
+        return self.value
+
+    def __str__(self) -> str:
+        return self.value
+
+    @classmethod
+    def list_values(cls) -> list[str]:
+        return [e.value for e in cls]
+
+    @classmethod
+    def list_names(cls) -> list[str]:
+        return [e.name for e in cls]
+
+    @classmethod
+    def get_name_by_value(cls, value: str) -> str | None:
+        for member in cls:
+            if member.value == value:
+                return member.name
+        return None
