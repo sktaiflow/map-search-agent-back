e75f5d50632b35255453ba2b145af4c27f1bf6c0
leejaehwan <smart0924@gmail.com>
2025-09-05 17:04:42 +0900

    temp: no chagne [TODO 주석]

--- app/container/graphs.py
+++ app/container/graphs.py
@@ -7,9 +7,8 @@ from app.graph.schema import Deps
 from app.graph.map_search_graph import MapSearchGraph
 
 
+# TODO: tool 기능이 명확화되면 -> graph에서 빠져야할 것들 빠져야함, memory saver 넣을지 말지 필요
 class GraphContainer(containers.DeclarativeContainer):
-    # Graph 인스턴스를 싱글톤으로 관리 -> Graph에서는 node에서 사용하는 llm, neo4j, tools주입.
-    # llm_container = providers.DependenciesContainer()
     client_container = providers.DependenciesContainer()
     neo4j_container = providers.DependenciesContainer()
     toolkit_container = providers.DependenciesContainer()
@@ -22,8 +21,8 @@ class GraphContainer(containers.DeclarativeContainer):
         postgres_db=pgvector_container.postgres_db,
         pgvector_models=pgvector_container.pgvector_models,
         neo4j_client=neo4j_container,
-        toolkit=toolkit_container.map_toolkit,
-        tools=toolkit_container.map_tools,
+        toolkit=toolkit_container.tool_executor,
+        tools=toolkit_container.openai_tools,
     )
 
     memory_saver = providers.Factory(MemorySaver)
