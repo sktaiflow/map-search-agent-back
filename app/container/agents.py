6b4ab5151a95a460d44981c00fb21a2b6284b38e
leejaehwan <smart0924@gmail.com>
2025-09-05 21:07:26 +0900

    chore: add TODO doc string

---
+++ app/container/agents.py
@@ -0,0 +1,28 @@
+from dependency_injector import containers, providers
+from app.clients.http_base import HTTPBaseClient
+from app.models.vectorstore.base import BaseModel as PGVectorModel
+
+from app.agents.map_search_agent import MapSearchAgent
+from app.container import GraphContainer
+
+
+# TODO: synonyhm api 호출을 graph에서 할거면 추후 제거 필요
+class AgentContainer(containers.DeclarativeContainer):
+    """
+    agentContainer Scope: 인풋 (전처리 [synonym], graph.invoke, graph.invoke 결과 후 처리)
+    """
+
+    graphs = providers.DependenciesContainer()
+    http_client = providers.DependenciesContainer()
+
+    map_agent = providers.Singleton(
+        MapSearchAgent,
+        graph=graphs.map_search_graph,
+        http_client=http_client.synonym_api,
+    )
+
+    factory = providers.Dict(
+        {
+            MapSearchAgent.config.agent_name: map_agent,
+        }
+    )
