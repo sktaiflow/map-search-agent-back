34e1824c0f2169e0b3166ec186feeb5833b9a60a
leejaehwan <smart0924@gmail.com>
2025-09-05 21:04:18 +0900

    feat: add request id for run_id of runnable config

---
+++ app/api/agents.py
@@ -0,0 +1,63 @@
+import asyncio
+from typing import Literal, Optional
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+
+from dependency_injector.wiring import Provide, inject
+
+# from ddtrace import tracer  # Optional: enable if tracer is used
+
+from app.container import Container
+from app.agents import MapSearchAgent
+from app.schemas.api.schema import InvokeRequest, InvokeResponse
+
+####
+from pydantic import BaseModel
+from typing import Dict, Any, Optional
+from app import logger
+from utils.request_handler import get_request_id
+
+###
+
+from langchain_core.runnables import RunnableConfig
+from app.graph.configuration import Configuration
+
+router = APIRouter(tags=["Map search Agent"])
+
+
+@router.post(
+    "/v1/invoke",
+    response_model=InvokeResponse,
+)
+@inject
+async def invoke_agent(
+    input_data: InvokeRequest,
+    agent: MapSearchAgent = Depends(Provide[Container.agents.map_agent]),
+) -> JSONResponse:
+
+    preprocess_input = (
+        " ".join(input_data.query) if isinstance(input_data.query, list) else input_data.query
+    )
+
+    graph_input_data = {
+        "user_id": input_data.user_id,
+        "query": input_data.query,
+        "query_synonym": preprocess_input,
+        "expand_search": input_data.expand_search,
+        "return_type": input_data.return_type,
+        "user_info": input_data.user_info,
+        "stream": False,
+    }
+
+    runnable_config = RunnableConfig(
+        run_id=get_request_id(),
+    )
+
+    runnable_config = RunnableConfig()
+    agent_response = await agent.ainvoke(
+        input_data=InvokeRequest.model_validate(graph_input_data), runnable_config=runnable_config
+    )
+
+    logger.info(f"agent_response: {agent_response}")
+
+    return JSONResponse(content=agent_response.model_dump())
