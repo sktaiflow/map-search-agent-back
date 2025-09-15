8d8b48a2a7fb9c6fcd00fea097317ba377469593
hazel-kim <hazel@sktai.io>
2025-09-15 16:08:24 +0900

    step2 : 기존 Neo4j Search Tool을 BaseTool로 변환, 수정

--- app/core/tools/search/__init__.py
+++ app/core/tools/search/__init__.py
@@ -1,6 +1,6 @@
-from app.core.tools.search.graph_tool import GraphSearchToolKit
+from app.core.tools.search.neo4j_search_tool import Neo4jSearchTool
 from app.core.tools.search.base import SearchBaseToolKit
-from app.core.tools.utils import SafeValidationTool
+from app.core.tools.base import StandardizedTool
 from langchain_core.utils.function_calling import convert_to_openai_function
 from typing import Any, Dict, List, Type, Optional, Union
 
@@ -18,8 +18,8 @@ from configs import config as global_config
 from app import logger
 
 
-class SearchToolkitCollectors(ToolKitCollectorProtocol[SafeValidationTool]):
-    _TOOLKIT_CLASSES: List[Type[SearchBaseToolKit]] = [GraphSearchToolKit]
+class SearchToolkitCollectors(ToolKitCollectorProtocol[StandardizedTool]):
+    _TOOLKIT_CLASSES: List[Type[SearchBaseToolKit]] = []
 
     def __init__(
         self,
@@ -39,20 +39,23 @@ class SearchToolkitCollectors(ToolKitCollectorProtocol[SafeValidationTool]):
         self.llm_embedding = llm_embedding
         self.cfg = cfg or global_config
 
-    def get_valid_tools(self) -> List[SafeValidationTool]:
+    def get_valid_tools(self) -> List[StandardizedTool]:
         """사용 가능한 tool 반환"""
-        valid_tool_list: List[SafeValidationTool] = []
-        for toolkit_class in self._TOOLKIT_CLASSES:
-            tools = toolkit_class(
-                self.llm,
-                self.graphmodel,
-                self.graph_db,
-                self.postgres_db,
-                self.vectormodel,
-                self.llm_embedding,
-                self.cfg,
-            ).valid_tools()
-            valid_tool_list.extend([tool for tool in tools if isinstance(tool, SafeValidationTool)])
+        valid_tool_list: List[StandardizedTool] = []
+        
+        # Neo4j Search Tool 직접 생성
+        neo4j_tool = Neo4jSearchTool(
+            llm=self.llm,
+            llm_embedding=self.llm_embedding,
+            neo4j_db=self.graph_db,
+            postgres_db=self.postgres_db,
+            graphmodel=self.graphmodel,
+            vectormodel=self.vectormodel,
+            cfg=self.cfg,
+        )
+        if neo4j_tool.status:
+            valid_tool_list.append(neo4j_tool)
+            
         return valid_tool_list
 
 
